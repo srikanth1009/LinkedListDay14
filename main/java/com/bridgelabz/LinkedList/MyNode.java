40923cda21e655a606fb1bac47e0ecd3928d4974
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-10-05 08:42:00 +0530

    Added:Ability to Create linked list for add

--- main/java/com/bridgelabz/LinkedList/MyNode.java
+++ main/java/com/bridgelabz/LinkedList/MyNode.java
@@ -1,18 +1,23 @@
 package com.bridgelabz.LinkedList;
-public class MyNode<K> {
+public class MyNode<K> implements INode<K> {
 	private K key;
-	private MyNode next;
+	private INode next;
 	public MyNode(K key) {
 		this.key = key;
 		this.next = null;
 	}
+	@Override
 	public K getKey() {
 		return key;
 	}
-	public MyNode getNext() {
+	@Override
+	public void setKey(K key) {
+		this.key = key;
+	}
+	public INode getNext() {
 		return next;
 	}
-	public void setNext(MyNode next) {
-		this.next = next;
+	public void setNext(INode next) {
+		this.next = (MyNode<K>) next;
 	}
 }
